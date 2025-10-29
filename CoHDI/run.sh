cfdb803b31b12be070cc919a42aeda93c2ec112d
Józwiak <arkadiusz.jozwiak@fujitsu.com>
2025-10-29 12:12:37 +0100

    [feature-2281]: Adjust run.sh and move it to the CoHDI dir

--- CoHDI/run.sh
+++ CoHDI/run.sh
@@ -1,14 +1,11 @@
-cd "CoHDI"
-rm -f Chart.lock
-kubectl delete ns composable-dra composable-resource-operator-system cohdi credentials-namespace --ignore-not-found || true
+#!/bin/bash
 
-helm dependency build .
-helm lint .
+# Cleanup previous configuration
+kubectl delete ns composable-dra composable-resource-operator-system cohdi credentials-namespace --ignore-not-found || true
+rm -f Chart.lock
 
 # Tag existing cluster-level resources so Helm will "treat them as its own"
-
 PREFIXES='^(composable-resource-operator-|cdi-|dynamic-device-scaler-)'
-
 for kind in clusterrole clusterrolebinding validatingwebhookconfiguration mutatingwebhookconfiguration; do
   kubectl get "$kind" -o jsonpath='{range .items[*]}{.metadata.name}{"\n"}{end}' \
   | grep -E "$PREFIXES" \
@@ -22,9 +19,15 @@ for kind in clusterrole clusterrolebinding validatingwebhookconfiguration mutati
     done
 done
 
+# Build dependencies
+helm dependency build .
 
+# Perform statuc analysis
+helm lint .
+
+# Install/upgrade cohdi
 helm upgrade --install cohdi . -n cohdi --create-namespace --debug
 
+# Wait and check pods
 sleep 5
-
 kubectl get pods -A
