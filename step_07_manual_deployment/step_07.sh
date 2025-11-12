f4c5a4c2cac44435ea017d38e79b451ce7e0df93
Józwiak <arkadiusz.jozwiak@fujitsu.com>
2025-11-12 12:44:15 +0100

    [feature-2281]: Setup_1017b tests

--- step_07_manual_deployment/step_07.sh
+++ step_07_manual_deployment/step_07.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-kubectl delete ns composable-dra composable-resource-operator-system
+kubectl delete ns composable-dra composable-resource-operator-system cohdi credentials-namespace --ignore-not-found || true
 kubectl create -f cdi-dra-namespace.yaml
 kubectl create -f cdi-dra-configmap.yaml
 kubectl apply -f cdi-dra-secret.yaml
@@ -9,4 +9,5 @@ kubectl apply -f generated-manifests-v5.yaml
 kubectl apply -f cdi-operator-secret.yaml
 kubectl apply -f dds.yaml
 
+sleep 5
 kubectl get pods -A
