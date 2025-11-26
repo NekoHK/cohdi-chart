ac3ee9f42a87b398d10f3a15e9c555815c4c05ce
Adam Wilkowski <adam.wilkowski@fujitsu.com>
2025-11-26 14:35:06 +0100

    Adjust pipeline

--- run_all_helm_tests.sh
+++ run_all_helm_tests.sh
@@ -9,18 +9,8 @@ if ! helm plugin list | grep -q 'unittest'; then
   helm plugin install https://github.com/helm-unittest/helm-unittest --verify=false
 fi
 
-echo "=== Running umbrella chart tests ==="
+echo "=== Running CoHDI chart tests ==="
 helm unittest ./CoHDI --strict || rc=$?
-
-echo "=== Running cdi_dra tests ==="
-helm unittest CoHDI/charts/cdi_dra --strict || rc=$?
-
-echo "=== Running cdi_operator tests ==="
-helm unittest CoHDI/charts/cdi_operator --strict || rc=$?
-
-echo "=== Running dds tests ==="
-helm unittest CoHDI/charts/dds --strict || rc=$?
 
 echo "=== Final exit code: $rc ==="
 exit $rc
-
