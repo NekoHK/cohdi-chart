0508bd45ea250e8805f12c6b0d72a4fe0b823dd8
Andrzej Frydrych <Andrzej.Frydrych@fujitsu.com>
2025-11-26 02:49:09 +0100

    fix plugin unitest instalation and run only for branch feature-2281-tests

--- run_all_helm_tests.sh
+++ run_all_helm_tests.sh
@@ -6,20 +6,20 @@ rc=0
 
 echo "=== Ensuring helm-unittest plugin is installed ==="
 if ! helm plugin list | grep -q 'unittest'; then
-  helm plugin install https://github.com/helm-unittest/helm-unittest
+  helm plugin install https://github.com/helm-unittest/helm-unittest --verify=false
 fi
 
 echo "=== Running umbrella chart tests ==="
-helm unittest ./CoHDI --color --strict || rc=$?
+helm unittest ./CoHDI --strict || rc=$?
 
 echo "=== Running cdi_dra tests ==="
-helm unittest CoHDI/charts/cdi_dra --color --strict || rc=$?
+helm unittest CoHDI/charts/cdi_dra --strict || rc=$?
 
 echo "=== Running cdi_operator tests ==="
-helm unittest CoHDI/charts/cdi_operator --color --strict || rc=$?
+helm unittest CoHDI/charts/cdi_operator --strict || rc=$?
 
 echo "=== Running dds tests ==="
-helm unittest CoHDI/charts/dds --color --strict || rc=$?
+helm unittest CoHDI/charts/dds --strict || rc=$?
 
 echo "=== Final exit code: $rc ==="
 exit $rc
